f05290a16175f9269f1c1d4bd588dc6b9846c3a5
Kimberly Henry <kimberly.henry@outlook.com>
2014-06-17 16:34:37 -0400

    Modified on BankAccount and TestBankAccount Moved to Problems Module

---
+++ Problems/src/Bank_Account/BankAccount.java
@@ -0,0 +1,60 @@
+package Bank_Account;
+
+/**
+ * Project:     Java Programming
+ * Package:     Bank_Account
+ * Filename:    BankAccount
+ * Created By:  Kimberly Henry
+ * On: 16-Jun-2014 At: 11:03 PM
+ * <p/>
+ * Create a class named BankAccount with fields that hold an account number, the owner’s name, and the account
+ * balance. Include a constructor that initializes each field to appropriate default values. Also include methods to
+ * get and set each of the fields. Include a method named deductMonthlyFee() that reduces the balance by $4.00.
+ * Include a static method named explainAccountPolicy() that explains that the $4 service fee will be deducted
+ * each month. Save the class as BankAccount.java.
+ */
+public class BankAccount {
+    private int accountNumber;
+    private String fullName;
+    private double accountBalance;
+
+    public BankAccount() {
+        accountNumber = 999999999;
+        fullName = " ";
+        accountBalance = 0;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        accountNumber = accountNumber;
+    }
+
+    public void setFullName(String fullName) {
+        fullName = fullName;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        accountBalance = accountBalance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void deductMonthlyFee() {
+        double monthlyFee = 4.00;
+        double newBalance = getAccountBalance() - monthlyFee;
+        System.out.println("Your balance after the monthly fees are " + newBalance);
+    }
+
+    public void explainAccountPolicy() {
+        System.out.println("The $4 service fee will be deducted each month.");
+    }
+}
--- Problems/src/Bank_Account/TestBankAccount.java
+++ Problems/src/Bank_Account/TestBankAccount.java
@@ -1,12 +1,14 @@
 package Bank_Account;
+
 import java.util.*;
+
 /**
  * Project:     Java Programming
  * Package:     Bank_Account
  * Filename:    TestBankAccount
  * Created By:  Kimberly Henry
  * On: 16-Jun-2014 At: 11:04 PM
- *
+ * <p/>
  * Create a class named TestBankAccount whose main() method declares four BankAccount objects. Call a getData()
  * method three times. Within the method, prompt a user for values for each field for a BankAccount, and return a
  * BankAccount object to the main() method where it is assigned to one of main()’s BankAccount  objects. Do not
@@ -15,22 +17,20 @@ import java.util.*;
  * the method that deducts the monthly fee, and displays the balance again. The showValues() method also calls
  * the method that explains the deduction policy. Save the  application as TestBankAccount.java.
  */
-public class TestBankAccount
-	{
-		public static void main(String[] args)
-			{
-				BankAccount test001 = new BankAccount();
-				BankAccount test002 = new BankAccount();
-				BankAccount test003 = new BankAccount();
-			}
-		public void getData()
-			{
-				Scanner enter = new Scanner(System.in);
-				System.out.println("Enter Bank Account Number: ");
-				BankAccount.se = enter.nextInt();
-				System.out.println("Enter your full name: ");
-				enter.nextLine();
-				System.out.println("Enter account balance: ");
-				enter.nextDouble();
-			}
-	}
+public class TestBankAccount {
+    public static void main(String[] args) {
+        BankAccount test001 = new BankAccount();
+        BankAccount test002 = new BankAccount();
+        BankAccount test003 = new BankAccount();
+    }
+
+    public void getData() {
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Enter Bank Account Number: ");
+        BankAccount.se = enter.nextInt();
+        System.out.println("Enter your full name: ");
+        enter.nextLine();
+        System.out.println("Enter account balance: ");
+        enter.nextDouble();
+    }
+}
