878c76b2b7b069deab39b5756cdd1938b450a19d
Kimberly Henry <kimberly.henry@outlook.com>
2014-06-17 21:32:35 -0400

    Modified and BankAccount Package.

--- Problems/src/Bank_Account/TestBankAccount.java
+++ Problems/src/Bank_Account/TestBankAccount.java
@@ -8,7 +8,9 @@ import java.util.*;
  * On: 16-Jun-2014 At: 11:04 PM
  *
  * Create a class named TestBankAccount whose main() method declares four BankAccount objects. Call a getData()
- * method three times. Within the method, prompt a user for values for each field for a BankAccount, and return a
+ * method three times.
+ * Within the method, prompt a user for values for each field for a BankAccount,
+ * and return a
  * BankAccount object to the main() method where it is assigned to one of main()’s BankAccount  objects. Do not
  * prompt the user for values for the fourth BankAccount object, but let it continue to hold the default values.
  * Then, in main(), pass each BankAccount object in turn to a showValues() method that displays the data, calls
@@ -26,8 +28,5 @@ public class TestBankAccount
 			}
 		public void getData()
 			{
-				Scanner enter = new Scanner(System.in);
-				System.out.print("Enter account number");
-				BankAccount.setAccountNumber(enter.nextInt());
 			}
 	}
