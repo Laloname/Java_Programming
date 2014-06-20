72d02626ae1a241f9345c0181f23de0450fd1d1e
Kimberly Henry <kimberly.henry@outlook.com>
2014-06-20 12:08:26 -0400

    close #7

--- Problems/src/Bird_Sighting/TestBirdSighting.java
+++ Problems/src/Bird_Sighting/TestBirdSighting.java
@@ -19,5 +19,10 @@ class TestBirdSighting
 				System.out.println("Number Seen:  " + test001.getNumberSeen());
 				test001.getDateSeen();
 				System.out.println("Day Of Year Seen:  " + test001.getDateSeen());
+				BirdSighting test002 = new BirdSighting("Blue Jay", 6, 2014, 6, 20);
+				System.out.println("Bird Species:  " + test002.getBirdSeen());
+				System.out.println("Number Seen:  " + test002.getNumberSeen());
+				test001.getDateSeen();
+				System.out.println("Day Of Year Seen:  " + test002.getDateSeen());
 			}
 	}
