e3cc7686e7eff6a350ec36d8170089af46daa6ac
Kimberly Henry <kimberly.henry@outlook.com>
2014-06-20 16:05:23 -0400

    close #18

---
+++ Chapter04/src/Next_Month/NextMonth.java
@@ -0,0 +1,26 @@
+package Next_Month;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+/**
+ * Project:  Java Programming
+ * Package:  Next_Month
+ * File:  NextMonth
+ * Created by Kimberly Henry <kimbelry.henry@outlook.com>
+ * On 20-Jun-2014
+ * At: 1:24 PM
+ *
+ * Write an application that uses methods in the GregorianCalendar class to
+ * calculate how many days are left until the first day of next month. Save the file as
+ * NextMonth.java.
+ */
+public class NextMonth
+	{
+		public static void main(String[] args)
+			{
+				GregorianCalendar today = new GregorianCalendar();
+				int currentDay = today.get(Calendar.DAY_OF_MONTH);
+				System.out.println("Today is the " + currentDay + "th day of the month.");
+				System.out.println("There is " + ((today.get(Calendar.DAY_OF_MONTH) + 1) -
+				                                  (today.getActualMaximum(Calendar.MONTH))) + " days left in the month.");
+			}
+	}
