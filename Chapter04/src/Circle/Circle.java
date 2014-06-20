c18bbdbd25d9cd884431e64d9d933ba141e78878
Kimberly Henry <kimberly.henry@outlook.com>
2014-06-19 23:06:35 -0400

    close #15

--- Chapter04/src/Circle/Circle.java
+++ Chapter04/src/Circle/Circle.java
@@ -7,12 +7,13 @@ package Circle;
  * On 19-Jun-2014
  * At: 10:43 PM
  *
- * Create a class named TestCircle whose main() method declares several
- * Circle objects. Using the setRadius() method, assign one Circle a small
- * radius value, and assign another a larger radius value. Do not assign a value to
- * the radius of the third circle; instead, retain the value assigned at construction.
- * Display all the values for all the Circle objects. Save the application as
- * TestCircle.java.
+ * Create a class named Circle with fields named radius, diameter, and area.
+ * Include a constructor that sets the radius to 1 and calculates the other two values.
+ * Also include methods named setRadius()and getRadius(). The setRadius()
+ * method not only sets the radius but also calculates the other two values. (The
+ * diameter of a circle is twice the radius, and the area of a circle is pi multiplied by
+ * the square of the radius. Use the Math class PI constant for this calculation.) Save
+ * the class as Circle.java.
  */
 public class Circle
 	{
--- Chapter04/src/Circle/TestCircle.java
+++ Chapter04/src/Circle/TestCircle.java
@@ -6,6 +6,13 @@ package Circle;
  * Created by Kimberly Henry <kimbelry.henry@outlook.com>
  * On 19-Jun-2014
  * At: 10:45 PM
+ *
+ * Create a class named TestCircle whose main() method declares several
+ * Circle objects. Using the setRadius() method, assign one Circle a small
+ * radius value, and assign another a larger radius value. Do not assign a value to
+ * the radius of the third circle; instead, retain the value assigned at construction.
+ * Display all the values for all the Circle objects. Save the application as
+ * TestCircle.java.
  */
 public class TestCircle
 	{
