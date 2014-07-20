95e8536796802fb5f6cdabc6479fe464e1f38f50
khenry312 <kimberly.henry@outlook.com>
2014-07-20 04:16:05 -0400

    Case Study Carlys Method

--- Carlys Catering/src/CarlysEventPriceWithMethods.java
+++ Carlys Catering/src/CarlysEventPriceWithMethods.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
  * Project: Carlys Catering
  * Package:
  * File:  CarlysEventPrice
- * Carly’s Catering provides meals for parties and special events. Write a program that
- * prompts the user for the number of guests attending an event and then computes the
- * total price, which is $35 per person. Display the company motto with the border that
- * you created in the CarlysMotto2 class in Chapter 1, and then display the number of
- * guests, price per guest, and total price. Also display a message that indicates true
- * or false depending on whether the job is classified as a large event—an event with
- * 50 or more guests. Save the file as CarlysEventPrice.java.
+ * Carly’s Catering provides meals for parties and special events. In Chapter 2,
+ * you wrote an application that prompts the user for the number of guests
+ * attending an event, displays the company motto with a border, and then
+ * displays the price of the event and whether the event is a large one. Now
+ * modify the program so that the main() method contains only three executable
+ * statements that each call a method as follows:
+ * 1. The first executable statement calls a public static int method that
+ * prompts the user for the number of guests and returns the value to the
+ * main() method.
+ * 2. The second executable statement calls a public static void method that
+ * displays the company motto with the border.
+ * 3. The last executable statement passes the number of guests to a public
+ * static void method that computes the price of the event, displays the
+ * price, and displays whether the event is a large event.
  */
 public class CarlysEventPriceWithMethods
 	{
--- Carlys Catering/src/Event.java
+++ Carlys Catering/src/Event.java
@@ -1,15 +1,59 @@
+import java.util.Scanner;
 /**
  * Created by Kimberly Henry <kimberly.henry@outlook.com>
  * On:  Jul at: 6:34 PM.
  * Project: Carlys Catering
  * Package: PACKAGE_NAME
  * File:  Event
+ * Create a class to hold Event data for Carly’s Catering. The class contains:
+ * 1. Two public final static fields that hold the price per guest ($35) and the
+ * cutoff value for a large event (50 guests)
+ * 2. Three private fields that hold an event number, number of guests for the
+ * event, and the price. The event number is stored as a String because Carly
+ * plans to assign event numbers such as M312.
+ * 3. Two public set methods that set the event number (setEventNumber()) and
+ * the number of guests (setGuests()). The price does not have a set method
+ * because the setGuests() method will calculate the price as the number of
+ * guests multiplied by the price per guest every time the number of guests is set.
+ * 4. Three public get methods that return the values in the three non-static fields
+ * Save the file as Event.java.
  */
 public class Event
 	{
+		public static final double PRICE_PER_GUEST = 35.00;
+		public static final int LARGE_EVENT_MAX = 50;
+		private String eventNumber;
+		private int guests;
+		private double price;
+		
 		//TODO and fields, constructors, and methods;
 		public static void main(String[] args)
 			{
 				//TODO add methods;
+			}
+		public void setEventNumber()
+			{
+				Scanner enter = new Scanner(System.in);
+				System.out.println("Enter the event number: ");
+				eventNumber = enter.nextLine();
+			}
+		public void setGuests(int guests)
+			{
+				Scanner enter = new Scanner(System.in);
+				System.out.println("Enter the number of guest attending ");
+				guests = enter.nextInt();
+				price =  guests * PRICE_PER_GUEST;
+			}
+		public String getEventNumber()
+			{
+				return eventNumber;
+			}
+		public int getGuests()
+			{
+				return guests;
+			}
+		public double getPrice()
+			{
+				return price;
 			}
 	}
